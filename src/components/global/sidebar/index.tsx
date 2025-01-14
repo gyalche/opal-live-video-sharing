ebb2ce8b007fca5649f46f98fa79aedb91dddf15
Dawa Sherpa <fupulamu01@gmail.com>
2025-01-14 22:45:10 +0545

    [FEAT]: sidebard workspace created

--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -1,17 +1,18 @@
 'use client'
-import { getWorkSpaces } from '@/actions/workspace'
+import { getNotifications, getWorkSpaces } from '@/actions/workspace'
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Separator } from '@/components/ui/separator'
 import { userQueryData } from '@/hooks/userQueryData'
-import { WorkspaceProps } from '@/types/index.types'
+import { NotificationProps, WorkspaceProps } from '@/types/index.types'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 import Modal from '../modal'
 import { PlusCircle } from 'lucide-react'
 import Search from '../search'
 import { MENU_ITEMS } from '@/constants'
 import SideBarItem from './sidebar-item'
+import WorkspacePlaceholder from './workspace-placeholder'
 
 type Props = {
   activeWorkspaceId: string,
@@ -19,16 +20,18 @@ type Props = {
 
 const Sidebar = ({activeWorkspaceId}: Props) => {
   const router = useRouter();
+  const pathName = usePathname();
+
   const {data, isFetched} = userQueryData(['user-workspaces'], getWorkSpaces);
-  
   const menuItems = MENU_ITEMS(activeWorkspaceId);
+  // const {data: notifications} = userQueryData(['user-notifications'], getNotifications)
   const { data: workspace} = data as WorkspaceProps;
+  // const { data: count } = notifications as NotificationProps;
 
   const onChangeActiveWorkSpace = (value:string) => {
     router.push(`/dashboard/${value}`)
   }
   const currentWorkspce = workspace.workspace.find((s) => s.id === activeWorkspaceId);
-
   return (
     <div className='bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden'>
       <div className='bg-[#111111] p-4 gap-2 flex justify-center items-center mb-4 absolute top-0 right-0 left-0'>
@@ -70,9 +73,59 @@ const Sidebar = ({activeWorkspaceId}: Props) => {
         <p className='w-full text-[#9D9D9D] font-bold mt-4'>
           <nav className='w-full'>
             <ul>
-              {menuItems?.map(() => (
-                <SideBarItem></SidebarItem>
+              {menuItems?.map((item) => (
+                <SideBarItem 
+                  href={item.href}
+                  icon={item.icon}
+                  selected={pathName === item.href}
+                  title={item.title}
+                  key={item.title}
+                  // notifications={
+                  //   (item.title === 'Notifications' && count._count && count._count.notification)
+                  //   || 0
+                  // }
+                />
               ))}
+            </ul>
+          </nav>
+          <Separator className='w-4/5'/>
+          <p className='w-full text-[#9D9D9D] font-bold mt-4'>Workspaces</p>
+          {workspace.workspace.length === 1 && 
+            workspace.members.length === 0 && <div className='w-full mt-[-1px]'>
+              <p className='text-[#9D9D9D] font-medium text-sm'>
+                  {workspace.subscription?.plan === 'FREE' ? 'Upgrate to create workspace' : 'No Workspaces'}
+                </p>  
+            </div>}
+          <nav className='w-full'>
+            <ul className='h-[150px] overflow-auto overflow-x-hidden fade-layer'>
+              {workspace.workspace.length > 0 && 
+                workspace.workspace.map((item) => item.type !== 'PERSONAL' && (
+                  <SideBarItem 
+                    href={`/dashboard/${item.id}`}
+                    selected={pathName === `/dashboard/${item.id}`}
+                    title={item.name}
+                    notifications={0}
+                    key={item.name}
+                    icon={<WorkspacePlaceholder>
+                      {item.name.charAt(0)}
+                    </WorkspacePlaceholder>}
+                  />
+                ))}
+
+                {
+                  workspace.members.length > 0 && workspace.members.map((item) => (
+                    <SideBarItem 
+                      href={`/dashboard/${item.Workspace.id}`}
+                      selected={pathName === `/dashboard/${item.Workspace.id}`}
+                      title={item.Workspace.name}
+                      notifications={0}
+                      key={item.Workspace.name}
+                      icon={<WorkspacePlaceholder>
+                        {item.Workspace.name.charAt(0)}
+                      </WorkspacePlaceholder>}
+                    />
+                  ))
+                }
             </ul>
           </nav>
         </p>
--- src/components/global/sidebar/sidebar-item.tsx
+++ src/components/global/sidebar/sidebar-item.tsx
@@ -24,7 +24,7 @@ const SideBarItem = ({href, icon, title, selected, notifications}: Props) => {
             {title}
           </span>
         </div>
-        
+        {}
       </Link>
     </li>
   )
